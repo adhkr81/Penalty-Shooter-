34172bcff1fc11979201ac975fbef5524dec67b7
Adriano Harm <adhkr81@gmail.com>
2022-06-11 23:27:55 -0300

    background image on

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 gameGrid = document.querySelector(".gameGrid");
 startBtn = document.querySelector("button");
+backgroundImg = document.querySelector("div");
 
 // console.log(startBtn);
 // console.log(gameGrid);
 
-
 startBtn.addEventListener("click", () => {
+	let game = new Game(gameGrid);
+	startBtn.disabled = true;
+	game.createGoalZones(); //CRIA DIVS E APPEND
+	game.goalKeeperLevel1(); //GOLEIRO RANDOM
+	backgroundImg.setAttribute("class", "backgroundImage");
 
-    let game = new Game(gameGrid);
-    startBtn.disabled = true
-    game.createGoalZones();  //CRIA DIVS E APPEND
-    game.goalKeeperLevel1(); //GOLEIRO RANDOM
-    let kickNumber = 0
-    
-    for (let i = 0; i < game.zonesIdArray.length; i++) { //LOOP COLOCAR EVENT NO BOTAO
-        let zoneBtn = document.getElementById(i + 1)
-        zoneBtn.addEventListener("click", (e) => {
-        game.kicks(e)
-        })
-    }
+	for (let i = 0; i < game.zonesIdArray.length; i++) {
+		//LOOP COLOCAR EVENT NO BOTAO
+		let zoneBtn = document.getElementById(i + 1);
+		zoneBtn.addEventListener("click", (e) => {
+			game.kicks(e);
+		});
+	}
 
-
-    console.log(`Esses sao os pulos do pre-definidos do goleiro ${game.goalKeeperJump}`)
-})
-
-
-
-
+	console.log(
+		`Esses sao os pulos do pre-definidos do goleiro ${game.goalKeeperJump}`
+	);
+});
