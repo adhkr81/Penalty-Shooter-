9ee55f6f53e87b9f29d0adcac9dcb1600960c1dc
Adriano Harm <adhkr81@gmail.com>
2022-06-12 18:22:26 -0300

    switch uniforms completed

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ class Game {
 		this.goalKeeperJump = [];
 		this.zonesIdArray = [];
 		this.roundsCounter = 0;
+		this.playerScoreArray = [];
+		this.computerScoreArray = [];
 	}
 
 	createGoalZones() {
@@ -19,8 +21,8 @@ class Game {
 		}
 	}
 
-	goalKeeperLevel1() {
-		for (let i = 0; i < 5; i++) {
+	computerLevel1() {
+		for (let i = 0; i < 10; i++) {
 			this.rng = Math.floor(Math.random() * 6 + 1); // RNG
 			this.goalKeeperJump.push(this.rng);
 		}
@@ -29,52 +31,119 @@ class Game {
 	kicks(e) {
 		this.kick = Number(e.currentTarget.id);
 		this.roundsCounter = this.roundsCounter + 1;
-		console.log(
-			`Jogador chutou no numero ${this.kick} no round ${this.roundsCounter}`
-		);
+ 
 		this.kickCompare();
 	}
+
 
 	kickCompare(index) {
 		if (this.roundsCounter === 1) {
 			if (this.kick !== this.goalKeeperJump[0]) {
-				console.log("GOOOOOOOOOOOL !");
-				// game.message.innerText = "GOOOOOOOOOOOL !"  COMO REFERENCIA ALGO FORA NO SCRIPT.JS???
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Jogador chutou em ${this.kick} e Computador pulou em ${0}`);
+				this.playerScoreArray.push(true)
 			} else {
-				console.log("DEFEEESAAAA !");
-				// game.message.innerText = "DEFEEESAAAA !"
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.playerScoreArray.push(false)
 			}
 		}
 
 		if (this.roundsCounter === 2) {
 			if (this.kick !== this.goalKeeperJump[1]) {
-				console.log("GOOOOOOOOOOOL !");
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Computador chutou em ${this.kick} e Playerpulou em ${1}`);
+				this.computerScoreArray.push(true)
 			} else {
-				console.log("DEFEEESAAAA !");
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.computerScoreArray.push(false)
 			}
 		}
 
 		if (this.roundsCounter === 3) {
 			if (this.kick !== this.goalKeeperJump[2]) {
-				console.log("GOOOOOOOOOOOL !");
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Jogador chutou em ${this.kick} e Computador pulou em ${2}`);
+				this.playerScoreArray.push(true)
 			} else {
-				console.log("DEFEEESAAAA !");
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.playerScoreArray.push(false)
 			}
 		}
 
 		if (this.roundsCounter === 4) {
 			if (this.kick !== this.goalKeeperJump[3]) {
-				console.log("GOOOOOOOOOOOL !");
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Computador chutou em ${this.kick} e Playerpulou em ${3}`);
+				this.computerScoreArray.push(true)
 			} else {
-				console.log("DEFEEESAAAA !");
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.computerScoreArray.push(false)
 			}
 		}
 
 		if (this.roundsCounter === 5) {
 			if (this.kick !== this.goalKeeperJump[4]) {
-				console.log("GOOOOOOOOOOOL !");
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Jogador chutou em ${this.kick} e Computador pulou em ${4}`);
+				this.playerScoreArray.push(true)
+
+
 			} else {
-				console.log("DEFEEESAAAA !");
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.playerScoreArray.push(false)
+
+			}
+		}
+		if (this.roundsCounter === 6) {
+			if (this.kick !== this.goalKeeperJump[5]) {
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Jogador chutou em ${this.kick} e Computador pulou em ${5}`);
+				this.computerScoreArray.push(true)
+			} else {
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.computerScoreArray.push(false)
+			}
+		}
+
+		if (this.roundsCounter === 7) {
+			if (this.kick !== this.goalKeeperJump[6]) {
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Computador chutou em ${this.kick} e Playerpulou em ${6}`);
+				this.playerScoreArray.push(true)
+			} else {
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.playerScoreArray.push(false)
+			}
+		}
+
+		if (this.roundsCounter === 8) {
+			if (this.kick !== this.goalKeeperJump[7]) {
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Jogador chutou em ${this.kick} e Computador pulou em ${7}`);
+				this.computerScoreArray.push(true)
+			} else {
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.computerScoreArray.push(false)
+			}
+		}
+
+		if (this.roundsCounter === 9) {
+			if (this.kick !== this.goalKeeperJump[8]) {
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Computador chutou em ${this.kick} e Playerpulou em ${8}`);
+				this.playerScoreArray.push(true)
+			} else {
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.playerScoreArray.push(false)
+			}
+		}
+
+		if (this.roundsCounter === 10) {
+			if (this.kick !== this.goalKeeperJump[9]) {
+				console.log(`ROUND ${this.roundsCounter} - GOOOOOOOOOOOL ! Jogador chutou em ${this.kick} e Computador pulou em ${9}`);
+				this.computerScoreArray.push(true)
+
+				console.log(this.playerScoreArray)
+				console.log(this.computerScoreArray)
+
+
+			} else {
+				console.log(`ROUND ${this.roundsCounter} - DEFEEESAAAA !`);
+				this.computerScoreArray.push(false)
+
+				console.log(`PLAYER SCORE${this.playerScoreArray}`)
+				console.log(`COMPUTER SCORE${this.computerScoreArray}`)
 			}
 		}
 	}
--- script.js
+++ script.js
@@ -2,6 +2,8 @@ gameGrid = document.querySelector(".gameGrid");
 startBtn = document.querySelector("button");
 backgroundImg = document.querySelector("section");
 message = document.getElementById("message");
+computer = document.getElementById("computer")
+player = document.getElementById("player")
 
 // console.log(startBtn);
 // console.log(gameGrid);
@@ -10,7 +12,7 @@ startBtn.addEventListener("click", () => {
 	let game = new Game(gameGrid);
 	startBtn.disabled = true;
 	game.createGoalZones(); //CRIA DIVS E APPEND
-	game.goalKeeperLevel1(); //GOLEIRO RANDOM
+	game.computerLevel1(); //GOLEIRO RANDOM
 	backgroundImg.setAttribute("class", "backgroundImage");
 
 
@@ -19,10 +21,23 @@ startBtn.addEventListener("click", () => {
 		let zoneBtn = document.getElementById(i + 1);
 		zoneBtn.addEventListener("click", (e) => {
 			game.kicks(e);
+			if (game.roundsCounter % 2 === 0) {
+				player.setAttribute("class", "playerRound1")
+				computer.setAttribute("class", "computerRound1")
+			} else {
+				player.setAttribute("class", "playerRound2")
+				computer.setAttribute("class", "computerRound2")
+			}
+		
 		});
 	}
 
 	console.log(
 		`Esses sao os pulos do pre-definidos do goleiro ${game.goalKeeperJump}`
 	);
+
+
+
+
+
 });
